cc07880dca29b026ccadffb4eb8fb8c6fb5d28a6
Elliot Thomas <eth0net@pm.me>
2024-06-22 16:14:08 +0100

    add mod solution and settings

---
+++ 1.5/Source/MoreCustomIdeoligions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace MoreCustomIdeoligions;
+
+/// <summary>
+/// The main class for the mod
+/// </summary>
+public class MoreCustomIdeoligions : Mod
+{
+    /// <summary>
+    /// The settings for the mod
+    /// </summary>
+    public MoreCustomIdeoligionsSettings settings;
+
+    /// <summary>
+    /// Constructor for the mod class to get the settings
+    /// </summary>
+    /// <param name="content"></param>
+    public MoreCustomIdeoligions(ModContentPack content) : base(content)
+    {
+        settings = GetSettings<MoreCustomIdeoligionsSettings>();
+    }
+
+    /// <summary>
+    /// Draw the settings window
+    /// </summary>
+    /// <param name="inRect"></param>
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        settings.DoSettingsWindowContents(inRect);
+        base.DoSettingsWindowContents(inRect);
+    }
+
+    /// <summary>
+    /// Add the settings category
+    /// </summary>
+    /// <returns></returns>
+    public override string SettingsCategory() => "MoreCustomIdeoligions".Translate();
+}
---
+++ 1.5/Source/MoreCustomIdeoligionsSettings.cs
@@ -0,0 +1,65 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MoreCustomIdeoligions;
+
+/// <summary>
+/// The settings for the mod
+/// </summary>
+public class MoreCustomIdeoligionsSettings : ModSettings
+{
+    // /// <summary>
+    // /// The global multiplier applied to the reaction speed
+    // /// </summary>
+    // public static float globalReactionSpeedMultiplier = 1f;
+
+    // /// <summary>
+    // /// The global multiplier applied to the reaction heat bonus
+    // /// </summary>
+    // public static float globalReactionHeatBonusMultiplier = 1f;
+
+    /// <summary>
+    /// Expose data to save/load
+    /// </summary>
+    public override void ExposeData()
+    {
+        // Scribe_Values.Look(ref globalReactionSpeedMultiplier, "globalReactionSpeedMultiplier", 1f);
+        // Scribe_Values.Look(ref globalReactionHeatBonusMultiplier, "globalReactionHeatBonusMultiplier", 1f);
+        base.ExposeData();
+    }
+
+    /// <summary>
+    /// Draw the settings window
+    /// </summary>
+    /// <param name="inRect"></param>
+    public void DoSettingsWindowContents(Rect inRect)
+    {
+        Listing_Standard listing_Standard = new();
+        listing_Standard.Begin(inRect);
+
+        // listing_Standard.Label("MoreCustomIdeoligions.GlobalReactionSpeedMultiplier".Translate() + ": " + globalReactionSpeedMultiplier);
+        // globalReactionSpeedMultiplier = listing_Standard.Slider(globalReactionSpeedMultiplier, 0.001f, 1000f);
+
+        // listing_Standard.Label("MoreCustomIdeoligions.GlobalReactionHeatBonusMultiplier".Translate() + ": " + globalReactionHeatBonusMultiplier);
+        // globalReactionHeatBonusMultiplier = listing_Standard.Slider(globalReactionHeatBonusMultiplier, 0.001f, 1000f);
+
+        listing_Standard.Gap();
+
+        if (listing_Standard.ButtonText("MoreCustomIdeoligions.ResetSettings".Translate()))
+        {
+            ResetSettings();
+        }
+
+        listing_Standard.End();
+    }
+
+    /// <summary>
+    /// Reset the settings to default
+    /// </summary>
+    public void ResetSettings()
+    {
+        // globalReactionSpeedMultiplier = 1f;
+        // globalReactionHeatBonusMultiplier = 1f;
+    }
+}
