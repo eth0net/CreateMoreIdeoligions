81ab9954c49b6ffb50b6d16d13246b1d9f8c1dfa
Elliot Thomas <eth0net@pm.me>
2024-06-22 16:23:25 +0100

    add harmony

---
+++ 1.5/Source/HarmonyPatcher.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace MoreCustomIdeoligions;
+
+[StaticConstructorOnStartup]
+public static class HarmonyPatcher
+{
+    static HarmonyPatcher()
+    {
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new("eth0net.MoreCustomIdeoligions");
+        harmony.PatchAll();
+    }
+}
