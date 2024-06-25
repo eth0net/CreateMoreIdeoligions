c9ca1762f01168bb1bef32449f762f0b95b4e4eb
Elliot Thomas <eth0net@pm.me>
2024-06-25 16:57:17 +0100

    separate patches

--- 1.5/Source/HarmonyPatch_IdeoUIUtility_DrawIdeoPlate.cs
+++ 1.5/Source/HarmonyPatch_IdeoUIUtility_DrawIdeoPlate.cs
@@ -32,33 +32,3 @@ static class HarmonyPatch_IdeoUIUtility_DrawIdeoPlate
         }
     }
 }
-
-[HarmonyPatch(typeof(IdeoUIUtility), "DrawIdeoRow")]
-static class HarmonyPatch_IdeoUIUtility_DrawIdeoRow
-{
-    private static readonly MethodInfo pawnInfo = AccessTools.PropertyGetter(typeof(MoreCustomIdeoligionsUtility), nameof(MoreCustomIdeoligionsUtility.Pawn));
-
-    private static readonly MethodInfo setPawnIdeoInfo = AccessTools.Method(typeof(MoreCustomIdeoligionsUtility), nameof(MoreCustomIdeoligionsUtility.SetPawnIdeo));
-
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        List<CodeInstruction> codes = new(instructions);
-
-        for (int i = 0; i < codes.Count; i++)
-        {
-            CodeInstruction code = codes[i];
-
-            if (code.opcode == OpCodes.Ldarg_0 && codes[i + 1].opcode == OpCodes.Stsfld)
-            {
-#if DEBUG
-                Log.Warning($"Patching DrawIdeoRow to update pawn ideo");
-#endif
-                yield return new CodeInstruction(OpCodes.Call, pawnInfo);
-                yield return new CodeInstruction(OpCodes.Ldarg_0);
-                yield return new CodeInstruction(OpCodes.Call, setPawnIdeoInfo);
-            }
-
-            yield return code;
-        }
-    }
-}
