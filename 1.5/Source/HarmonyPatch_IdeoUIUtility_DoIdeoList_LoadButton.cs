e21b85bd28b0ad04a29a157786b972f63e9cd354
Elliot Thomas <eth0net@pm.me>
2024-06-28 00:48:29 +0100

    fix second ideo load button

--- 1.5/Source/HarmonyPatch_IdeoUIUtility_DoIdeoList_LoadButton.cs
+++ 1.5/Source/HarmonyPatch_IdeoUIUtility_DoIdeoList_LoadButton.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
@@ -9,27 +10,30 @@ namespace CreateMoreIdeoligions;
 [HarmonyPatch]
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 [SuppressMessage("ReSharper", "UnusedType.Global")]
-internal static class HarmonyPatch_IdeoUIUtility_DoIdeoList_LoadButton
+internal static class HarmonyPatch_IdeoUIUtility_LoadButtons
 {
     [SuppressMessage("ReSharper", "UnusedMember.Local")]
-    private static MethodBase TargetMethod()
+    private static IEnumerable<MethodBase> TargetMethods()
     {
-        // The method is tucked away in a nested class, so we need to find it first
-        // It's defined as a delegate in IdeoUIUtility.DoIdeoList for the Load button
-        // In 1.5, the method is in IdeoUIUtility.<>c__DisplayClass61_0.<DoIdeoList>b__1
-        return (from type in typeof(IdeoUIUtility).GetNestedTypes(AccessTools.all)
+        // The methods are tucked away in nested classes, so we need to find them first
+        // One is defined as a delegate in IdeoUIUtility.DoIdeoList for the load button in the list
+        // The other is defined as a delegate in IdeoUIUtility.DoInitialIdeoSelection for the central load button
+        // In 1.5, the first method is IdeoUIUtility.<>c__DisplayClass61_0.<DoIdeoList>b__1
+        // and the second is IdeoUIUtility.<>c.<DoInitialIdeoSelection>b__59_0
+        return from type in typeof(IdeoUIUtility).GetNestedTypes(AccessTools.all)
             from method in type.GetMethods(AccessTools.all)
-            where method.Name.Contains("DoIdeoList") && method.ReturnType == typeof(void)
+            where method.Name.Contains("DoIdeoList") || method.Name.Contains("DoInitialIdeoSelection")
+            where method.ReturnType == typeof(void)
             let parameters = method.GetParameters()
             where parameters.Length == 1 && parameters[0].ParameterType == typeof(Ideo)
-            select method).FirstOrDefault();
+            select method;
     }
 
     [SuppressMessage("ReSharper", "UnusedMember.Local")]
     private static void Postfix(Ideo ideo)
     {
 #if DEBUG
-        Log.Warning($"[<DoIdeoList>b__1] Adding custom ideo: {ideo}");
+        Log.Warning($"[Ideo Load Button] Adding custom ideo: {ideo}");
 #endif
         CreateMoreIdeoligionsUtility.AddCustomIdeo(ideo);
     }
